c938d659d171e38bdd5104ba6c54b6bd8b704423
BeastBoy2003 <114703199+BeastBoy2003@users.noreply.github.com>
2025-03-25 17:11:49 -0400

    Update ContentView.swift

--- StoreKit Project/StoreKit Project/ContentView.swift
+++ StoreKit Project/StoreKit Project/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showList = false
+    @State private var items = ["Item 1", "Item 2", "Item 2"]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to our Card Game!")
+            
+            Button(showList ? "hide List" : "Show List") {
+                showList.toggle()
+            }
+            .padding()
+            .background(Color.black)
+            .foregroundColor(.white)
+            .cornerRadius(30)
+        
+            if showList {
+                List(items, id: \.self) { item in Text(item)
+                }
+                .transition(.slide)
+                .animation(.easeInOut, value: showList)
+            }
+            
         }
         .padding()
     }
