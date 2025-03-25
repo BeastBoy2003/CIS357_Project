2b3d9776360ac3c952126763000b9eecadb9131e
BeastBoy2003 <114703199+BeastBoy2003@users.noreply.github.com>
2025-03-25 16:13:07 -0400

    Add files via upload

---
+++ StoreKit Project/StoreKit Project/StoreKit_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit_ProjectApp.swift
+//  StoreKit Project
+//
+//  Created by Jacob D. Sanchez-Puentes on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoreKit Project/StoreKit ProjectTests/StoreKit_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit_ProjectTests.swift
+//  StoreKit ProjectTests
+//
+//  Created by Jacob D. Sanchez-Puentes on 3/25/25.
+//
+
+import Testing
+@testable import StoreKit_Project
+
+struct StoreKit_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
