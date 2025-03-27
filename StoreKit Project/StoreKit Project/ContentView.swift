76a6456ca3ef660090219fab6a715423fd1efecc
BeastBoy2003 <114703199+BeastBoy2003@users.noreply.github.com>
2025-03-27 16:31:00 -0400

    Update ContentView.swift

--- StoreKit Project/StoreKit Project/ContentView.swift
+++ StoreKit Project/StoreKit Project/ContentView.swift
@@ -10,31 +10,40 @@ import SwiftUI
 struct ContentView: View {
     @State private var showList = false
     @State private var items = ["Item 1", "Item 2", "Item 2"]
-    
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Welcome to our Card Game!")
-            
-            Button(showList ? "Back" : "Click To Play") {
-                showList.toggle()
-            }
-            .padding()
-            .background(Color.black)
-            .foregroundColor(.white)
-            .cornerRadius(30)
         
-            if showList {
-                List(items, id: \.self) { item in Text(item)
-                }
-                .transition(.slide)
-                .animation(.easeInOut, value: showList)
-            }
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.green, .blue]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
             
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Welcome to our Card Game!")
+                    .foregroundColor(.white)
+                
+                Button(showList ? "Back" : "Click To Play") {
+                    showList.toggle()
+                }
+                .padding()
+                .background(Color.black)
+                .foregroundColor(.white)
+                .cornerRadius(30)
+                
+                if showList {
+                    List(items, id: \.self) { item in Text(item)
+                    }
+                    .transition(.slide)
+                    .animation(.easeInOut, value: showList)
+                }
+            }
+
         }
-        .padding()
     }
 }
 
